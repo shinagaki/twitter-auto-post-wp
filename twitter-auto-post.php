0870ea00376f1cc153cc1f51dad549ef8bf0aed3
Shintaro Inagaki <shinagaki@gmail.com>
2025-09-09 09:31:00 +0900

    fix: v1.1.1 - 投稿済み記事の再投稿防止を強化
    - 投稿済み記事のチェックボックスを無効化（disabled）
    - 誤って再投稿されるバグを修正
    - ユーザビリティの向上

--- twitter-auto-post.php
+++ twitter-auto-post.php
@@ -3,7 +3,7 @@
  * Plugin Name: Twitter/X Auto Post
  * Plugin URI: https://github.com/shinagaki/twitter-auto-post-wp
  * Description: WordPressの記事投稿時に自動的にTwitter/Xにも投稿するプラグイン。リンクカード表示にも対応
- * Version: 1.1.0
+ * Version: 1.1.1
  * Author: Shintaro Inagaki
  * Author URI: https://creco.net/
  * License: GPL v2 or later
@@ -23,7 +23,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 // プラグインの定数を定義
-define( 'TWITTER_AUTO_POST_VERSION', '1.1.0' );
+define( 'TWITTER_AUTO_POST_VERSION', '1.1.1' );
 define( 'TWITTER_AUTO_POST_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 define( 'TWITTER_AUTO_POST_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 
@@ -732,7 +732,8 @@ class TwitterAutoPost {
 
 		echo '<div style="margin: 10px 0;">';
 		echo '<label style="display: flex; align-items: center; gap: 8px;">';
-		echo '<input type="checkbox" name="twitter_manual_post" value="1" ' . checked( $manual_control, '1', false ) . '>';
+		$disabled = ! empty( $already_posted ) ? ' disabled' : '';
+		echo '<input type="checkbox" name="twitter_manual_post" value="1" ' . checked( $manual_control, '1', false ) . $disabled . '>';
 		echo '<span>Twitter/Xに投稿する</span>';
 		echo '</label>';
 
